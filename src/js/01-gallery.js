8204ca73d03f8812181ca202920a84474bc688bb
MariaDekhtiaruk <109179342+MariaDekhtiaruk@users.noreply.github.com>
2022-11-26 13:37:42 +0200

    Commit 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// console.log(galleryItems);
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryWrapper = document.querySelector(`.gallery`);
+const previewImages = createGaleryImg(galleryItems);
+galleryWrapper.insertAdjacentHTML('beforeend', previewImages);
+let instance = null;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionSelector: `img`,
+  captionPosition: `bottom`,
+  captionDelay: 250,
+});
+
+function onEscapePress(evt) {
+  console.log(evt.key);
+  if (evt.key === 'Escape') {
+    instance.close();
+  }
+}
+
+function createGaleryImg(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src= "${preview}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join('');
+}
