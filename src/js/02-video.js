61d8442071f338f6052e6c06dae92f46eeac9d11
MariaDekhtiaruk <109179342+MariaDekhtiaruk@users.noreply.github.com>
2022-11-26 14:14:13 +0200

    Add Vimeo player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+console.log('iframe', iframe);
 
+const player = new Player(iframe);
+
+console.log('player', player);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+const onTimeupdate = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', onTimeupdate);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
