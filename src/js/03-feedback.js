210e14956e73ddde731617a2743fc6c9b4abd99b
MariaDekhtiaruk <109179342+MariaDekhtiaruk@users.noreply.github.com>
2022-11-26 16:24:23 +0200

    Input reset

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+const form = document.querySelector(`.feedback-form`);
+form.addEventListener(`submit`, onFormSubmit);
 
+function onFormSubmit(evt) {
+  event.preventDefault();
+
+  const formElements = evt.currentTarget.elements;
+  const mail = formElements.email.value;
+  const message = formElements.message.value;
+
+  const formData = {
+    mail,
+    message,
+  };
+  console.log(formData);
+  form.reset();
+  localStorage.clear();
+}
+
+const onFormInput = evt => {
+  let formState = localStorage.getItem('feedback-form-state');
+  if (!formState) {
+    formState = {
+      email: ``,
+      message: ``,
+    };
+  } else {
+    formState = JSON.parse(formState);
+  }
+  formState[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+};
+
+form.addEventListener('input', onFormInput);
+//const currentTime = localStorage.getItem('videoplayer-current-time');
